284ba4225b8cb1dba822e076b013e400e6611c6b
ILeshiy <153270138+ILeshiy@users.noreply.github.com>
2025-04-29 01:04:45 -0700

    updt

---
+++ unit2/Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+    private GameObject gameOverText;
+
+    private void Start()
+    {
+
+        gameOverText = GameObject.Find("GameOver");
+
+        if (gameOverText == null)
+        {
+            Debug.LogWarning("game over not found");
+        }
+        else
+        {
+            gameOverText.SetActive(false); 
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            if (gameOverText != null)
+            {
+                gameOverText.SetActive(true);
+                Debug.Log("Text was shown");
+            }
+
+            Time.timeScale = 0f;
+            Invoke("ResetScene", 2f);
+            Debug.Log("needed log");
+        }
+
+        Destroy(gameObject);
+        Debug.Log("Object Was Destroyed");
+    }
+
+    void ResetScene()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
